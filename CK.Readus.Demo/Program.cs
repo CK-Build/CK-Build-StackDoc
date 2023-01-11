2853376eebd7e2fa263274e8f965a5bc7bc4d34d
Aymeric Richard <aymeric.richard@signature-code.com>
2023-01-11 17:08:04 +0100

    ✨ DocumentationCrawler can GetMarkdownFiles fullPath within a Path :
    Alongside with a unit test and a demo console app for further testing.
    Those 2 are configured with log text and console (I like it when I dev).

--- CK.Readus.Demo/Program.cs
+++ CK.Readus.Demo/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine( "Hello, World!" );
+using CK.Core;
+using CK.Monitoring;
+using CK.Monitoring.Handlers;
+using CK.Readus;
+using Console = System.Console;
+
+var grandOutputConfiguration = new GrandOutputConfiguration().AddHandler( new ConsoleConfiguration() );
+GrandOutput.EnsureActiveDefault( grandOutputConfiguration );
+
+var monitor = new ActivityMonitor();
+
+var path = @"C:\Users\Aymeric.Richard\Downloads\CK-Core-develop";
+
+var crawler = new DocumentationCrawler();
+var files = crawler.GetMarkdownFiles( monitor, path ).ToArray();
+
+Console.WriteLine( $"{files.Length} md files :" );
+foreach( var file in files )
+{
+    Console.WriteLine( file );
+}
+
+Console.ReadKey();
---
+++ Tests/CK.Readus.Tests/DocumentationCrawlerTests.cs
@@ -0,0 +1,61 @@
+﻿using CK.Core;
+
+namespace CK.Readus.Tests;
+
+public class DocumentationCrawlerTests
+{
+    private readonly NormalizedPath _inFolder;
+    private readonly NormalizedPath _fooBarSolutionFolder;
+    private int _fooBarMdFileCount;
+
+    public DocumentationCrawlerTests()
+    {
+        _inFolder = TestHelper.TestProjectFolder.AppendPart( "In" );
+        var tempFolder = _inFolder.AppendPart( "Temp" );
+        _fooBarSolutionFolder = tempFolder.AppendPart( "FooBar" );
+
+        Directory.CreateDirectory( _inFolder );
+    }
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        if( Directory.Exists( _fooBarSolutionFolder ) )
+            Directory.Delete( _fooBarSolutionFolder, true );
+
+        // Setup FooBar Solution.
+        var filesNames = new[]
+        {
+            "Readme.md",
+            "code.cs",
+            "FooBar.sln",
+            ".hiddenFile",
+            "muffin-recipe.docx",
+            "SubFolder1/Readme.md",
+            "SubFolder1/someCode.cs",
+            "SubFolder1/SubFolder.csproj",
+            "SubFolder1/bin/Debug/library.dll",
+            "SubFolder2/Readme.md",
+            "SubFolder2/Hello/There/This/Is/General/Kenobi.md",
+        };
+
+        foreach( var fileName in filesNames )
+        {
+            var file = _fooBarSolutionFolder.Combine( new NormalizedPath( fileName ) );
+
+            Directory.CreateDirectory( file.RemoveLastPart() );
+            File.Create( file );
+        }
+
+        _fooBarMdFileCount = 4;
+    }
+
+    [Test]
+    public void GetMarkdownFiles_should_get_md_files()
+    {
+        var sut = new DocumentationCrawler();
+        var result = sut.GetMarkdownFiles( TestHelper.Monitor, _fooBarSolutionFolder );
+
+        result.Count().Should().Be( _fooBarMdFileCount );
+    }
+}
