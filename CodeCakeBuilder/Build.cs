1106b254c63a3a12c0ac0659570fa927fcead483
Aymeric Richard <aymeric.richard@signature-code.com>
2023-01-16 18:45:30 +0100

    👷 Add CodeCakeBuilder and AssemblyInfo.

---
+++ CodeCakeBuilder/Build.cs
@@ -0,0 +1,71 @@
+using Cake.Common.IO;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+
+namespace CodeCake
+{
+    public partial class Build : CodeCakeHost
+    {
+        public Build()
+        {
+            Cake.Log.Verbosity = Verbosity.Diagnostic;
+
+            StandardGlobalInfo globalInfo = CreateStandardGlobalInfo()
+                                                .AddDotnet()
+                                                .SetCIBuildTag();
+
+            Task( "Check-Repository" )
+                .Does( () =>
+                {
+                    globalInfo.TerminateIfShouldStop();
+                } );
+                
+            Task( "Clean" )
+                .IsDependentOn( "Check-Repository" )
+                .Does( () =>
+                {
+                    globalInfo.GetDotnetSolution().Clean();
+                    Cake.CleanDirectories( globalInfo.ReleasesFolder.ToString() );
+                    Cake.DeleteFiles( "Tests/**/TestResult*.xml" );
+                } );
+
+            Task( "Build" )
+                .IsDependentOn( "Check-Repository" )
+                .IsDependentOn( "Clean" )
+                .Does( () =>
+                {
+                    globalInfo.GetDotnetSolution().Build();
+                } );
+
+            Task( "Unit-Testing" )
+                .IsDependentOn( "Build" )
+                .WithCriteria( () => Cake.InteractiveMode() == InteractiveMode.NoInteraction
+                                     || Cake.ReadInteractiveOption( "RunUnitTests", "Run Unit Tests?", 'Y', 'N' ) == 'Y' )
+                .Does( () =>
+                {
+                    globalInfo.GetDotnetSolution().Test();
+                } );
+
+            Task( "Create-NuGet-Packages" )
+                .WithCriteria( () => globalInfo.IsValid )
+                .IsDependentOn( "Unit-Testing" )
+                .Does( () =>
+                {
+                    globalInfo.GetDotnetSolution().Pack();
+                } );
+
+            Task( "Push-Artifacts" )
+                .IsDependentOn( "Create-NuGet-Packages" )
+                .WithCriteria( () => globalInfo.IsValid )
+                .Does( async () =>
+                {
+                    await globalInfo.PushArtifactsAsync();
+                } );
+
+            // The Default task for this script can be set here.
+            Task( "Default" )
+                .IsDependentOn( "Push-Artifacts" );
+        }
+
+    }
+}
