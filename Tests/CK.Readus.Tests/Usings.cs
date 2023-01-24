6d6d4d9377c818e8f7b5826ecb0627ef876cf1f3
Aymeric Richard <aymeric.richard@signature-code.com>
2023-01-24 16:47:46 +0100

    🧑‍💻 Displays Path on debugger.

--- Tests/CK.Readus.Tests/Usings.cs
+++ Tests/CK.Readus.Tests/Usings.cs
@@ -1,3 +1,7 @@
 global using NUnit.Framework;
 global using FluentAssertions;
 global using static CK.Testing.MonitorTestHelper;
+using System.Diagnostics;
+using CK.Core;
+
+[assembly: DebuggerDisplay( "{Path}", Target = typeof( NormalizedPath ) )]
