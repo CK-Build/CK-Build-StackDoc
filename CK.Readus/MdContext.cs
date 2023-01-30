6b93811926cc3d0214aa306b7fefc2aef05ae75b
Aymeric Richard <aymeric.richard@signature-code.com>
2023-01-30 19:43:20 +0100

    From a MdDocument get related actions to run.
    Could be configurable in the future.

--- CK.Readus/MdContext.cs
+++ CK.Readus/MdContext.cs
@@ -1,4 +1,6 @@
-﻿namespace CK.Readus;
+﻿using CK.Core;
+
+namespace CK.Readus;
 
 // If the name can me Md[..]Context with [..] being after "Stack" alphabetically,
 // the solution explorer will display all the components in the right order.
@@ -7,4 +9,35 @@ public class MdContext
 {
     private IDictionary<string, MdStack> Stacks { get; }
     private NormalizedPath _virtualRoot = new NormalizedPath( "~" );
+
+    private Action<IActivityMonitor, NormalizedPath>[] GetChecks(MdDocument mdDocument)
+    {
+        var mdRepository = mdDocument.Parent;
+        var mdStack = mdRepository.Parent;
+
+        var repoCheck = mdRepository.CheckRepository;
+        var stackCheck = mdStack.CheckStack;
+
+        var checks = new[] { repoCheck, stackCheck};
+
+        return checks;
+    }
+
+    private Func<IActivityMonitor, NormalizedPath, NormalizedPath>[] GetTransforms(MdDocument mdDocument)
+    {
+        var mdRepository = mdDocument.Parent;
+        var mdStack = mdRepository.Parent;
+
+        var repoCheck = mdRepository.TransformRepository;
+        var stackCheck = mdStack.TransformCrossRepositoryUrl;
+
+        var checks = new[] { repoCheck, stackCheck};
+
+        return checks;
+    }
+
+    // ResolveLinks() // Apply all transformations
+
+    // path ? to output.
+    // Output type, like tree, one directory etc.
 }
