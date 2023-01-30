5887f1d602995d42df3676aaa3ee830c9ac3ca8a
Aymeric Richard <aymeric.richard@signature-code.com>
2023-01-30 19:08:14 +0100

    Add Monitor Helper.

--- Tests/CK.Readus.Tests/TestBase.cs
+++ Tests/CK.Readus.Tests/TestBase.cs
@@ -1,7 +1,11 @@
-﻿namespace CK.Readus.Tests;
+﻿using CK.Core;
+
+namespace CK.Readus.Tests;
 
 public class TestBase
 {
+    public IActivityMonitor Monitor => TestHelper.Monitor;
+
     // ReSharper disable once MemberCanBeProtected.Global
     public TestBase(  )
     {
